032fae2744e965601e8b5bffe354a1db38434471
ismaeldiazy <ismaeldiazy@gmail.com>
2020-04-29 18:42:36 +0200

    Added link field to the project model. Showed on the template with a if tag (if the link exists).

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to="projects")
+    link = models.URLField(null=True, blank=True)
     # Created date
     created = models.DateTimeField(auto_now_add=True)
     # Modification date 
